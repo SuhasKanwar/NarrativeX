2bb1235de9c98107ae6ceefa0086643ad1c4addc
SuhasKanwar <suhas.kanwar@gmail.com>
2026-09-19 02:17:33 +0530

    server(db): ai chat system

--- server/src/prisma/enums.ts
+++ server/src/prisma/enums.ts
@@ -4,3 +4,23 @@ export const AuthProvider = {
 } as const;
 
 export type AuthProvider = (typeof AuthProvider)[keyof typeof AuthProvider];
+
+export const Sender = {
+  user: "user",
+  bot: "bot",
+  system: "system",
+} as const;
+
+export type Sender = (typeof Sender)[keyof typeof Sender];
+
+export const ChatType = {
+  text: "text",
+} as const;
+
+export type ChatType = (typeof ChatType)[keyof typeof ChatType];
+
+export const ConversationVariant = {
+  chat: "chat",
+} as const;
+
+export type ConversationVariant = (typeof ConversationVariant)[keyof typeof ConversationVariant];
